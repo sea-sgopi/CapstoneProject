b2dcb224eeb274618a07512955e00e1b82b2ad75
S Gopi <skopeemail@gmail.com>
2024-08-30 21:23:53 +0530

    pre commit & lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals:{
+    ...globals.browser,
+    ...globals.node,
+    ...globals.jest
+  }}},
+  pluginJs.configs.recommended,
+  {
+    plugins: {
+      jest: pluginJest,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginJest.configs.recommended.rules,
+    }
+  }
+];
